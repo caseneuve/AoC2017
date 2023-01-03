8994a8636dd0f4c92c527747270e3295cec96854
Piotr Kaznowski <piotr@kazno.dev>
2023-01-03 20:38:55 +0100

    Refactor day 2

--- day02/solution.clj
+++ day02/solution.clj
@@ -9,7 +9,7 @@
   (->> it ((juxt #(apply min %) #(apply max %))) (apply -) abs (+ sum)))
 
 (defn eavenly-div [sum it]
-  (->> it (map #(sort > %)) (filter #(zero? (apply mod %))) first (apply /) (+ sum)))
+  (->> it (map #(sort > %)) (drop-while #(not= 0 (apply mod %))) first (apply /) (+ sum)))
 
 (def solve #(reduce %2 0 %1))
 
@@ -20,8 +20,8 @@
 
 
 (comment
-  (let [test-input {:1 ["5 1 9 5\n7 5 3\n2 4 6 8" 18],
-                    :2 ["5 9 2 8\n9 4 7 3\n3 8 6 5" 9]}]
+  (let [test-input {:1 ["5 1 9 5\n7 5 3\n2 4 6 8",  18],
+                    :2 ["5 9 2 8\n9 4 7 3\n3 8 6 5", 9]}]
     (for [[p [it exp]] test-input
           :let [it (cond->> (parse it) (= p :2) (map #(combinations % 2))),
                 f (p {:1 minmax-diff :2 eavenly-div})]]
