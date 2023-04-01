fd3c52b2222670a0047673ca6ef56f353fd249ef
Piotr Kaznowski <piotr@kazno.dev>
2023-04-01 23:05:40 +0200

    Day 18: part 2 + refactored 1

--- Clojure/day18.clj
+++ Clojure/day18.clj
@@ -1,26 +1,48 @@
 (ns day18
   (:require [input :refer [f->lines]]))
 
-(defn parse [it] (mapv #(re-seq #"-?\w+" %) it))
+(defn ops [prog [op x y]]
+  (let [value #(if-let [n (and % (parse-long %))] n (prog %)), y (value y)]
+    (case op "jgz" (if (> (value x) 0) (update prog :pos + y) (update prog :pos inc))
+          (-> (case op
+                ;; part 1 only:
+                "snd" (assoc prog :freq (prog x))
+                "rcv" (cond-> prog (not= 0 x) (assoc :last (prog :freq)))
+                ;; common:
+                "set" (assoc  prog x y)
+                "add" (update prog x + y)
+                "mul" (update prog x * y)
+                "mod" (update prog x mod y))
+              (update :pos inc)))))
 
-(defn reg-or-num [rx y]
-  (if-let [c (and y (re-matches #"-?\d+" y))] (parse-long c) (rx y)))
+(defn run1 [instructions]
+  (loop [prog (merge (zipmap (map second instructions) (repeat 0)) {:pos 0})]
+    (if-let [l (prog :last)] l (recur (ops prog (instructions (prog :pos)))))))
 
-(def ops
-  {"snd" (fn [rx x _] (assoc rx :freq (rx x)))
-   "rcv" (fn [rx x _] (cond-> rx (not= 0 (rx x)) (assoc :last (rx :freq))))
-   "jgz" (fn [rx x y] (if-not (zero? (rx x)) (update rx :i + y) (update rx :i inc)))
-   "set" (fn [rx x y] (assoc rx x y))
-   "add" (fn [rx x y] (update rx x + y))
-   "mul" (fn [rx x y] (update rx x * y))
-   "mod" (fn [rx x y] (update rx x rem y))})
-
-(defn part1 [it]
-  (loop [rx (conj (zipmap (map second it) (repeat 0)) {:i 0})]
-    (if-let [l (rx :last)] l
-            (let [[o x y] (it (rx :i)), y (reg-or-num rx y)]
-              (recur (cond-> ((ops o) rx x y) (not= o "jgz") (update :i inc)))))))
+(defn run2 [instructions]
+  (let [init {:pos 0, "p" 0, :sent 0, :queue (clojure.lang.PersistentQueue/EMPTY)}
+        A (merge (zipmap (map second instructions) (repeat 0)) init)
+        B (merge A {"p" 1})]
+    (loop [duet [A B], [this other :as order] [0 1]]
+      (if (and (get-in duet [0 :waiting?]) (empty? (get-in duet [1 :queue]))
+               (get-in duet [1 :waiting?]) (empty? (get-in duet [0 :queue])))
+        (get-in duet [1 :sent])
+        (let [current (duet this), [op x _ :as instruction] (instructions (current :pos))]
+          (case op
+            "snd" (recur (-> duet
+                             (update-in [this :queue] conj (current x)) (update-in [this :sent] inc)
+                             (update-in [this :pos] inc))
+                         order)
+            "rcv" (let [q (get-in duet [other :queue])]
+                    (if (empty? q)
+                      (recur (assoc-in duet [this :waiting?] true) (reverse order))
+                      (recur (-> duet
+                                 (assoc-in [this x] (peek q)) (update-in [other :queue] pop)
+                                 (assoc-in [this :waiting?] false)
+                                 (update-in [this :pos] inc))
+                             order)))
+            (recur (assoc duet this (ops current instruction)) order)))))))
 
 (defn -main [day]
-  (let [input (->> day f->lines parse)]
-    {:part1 (part1 input) :part2 nil}))
+  (let [input (->> day f->lines (mapv #(re-seq #"-?\w+" %)))]
+    {:part1 (run1 input) :part2 (run2 input)}))
